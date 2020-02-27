7ecb167181a2ff0141bc326fdee2960a6c45b2b3
Kay Evans-Stocks <kay.stocks@rogers.com>
2020-02-27 12:38:57 -0500

    Updates

---
+++ precache-manifest.6b1ec743992cd2bff01de47f5056232c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e95f41f63e2b9382168234e4c6cba3",
+    "url": "/pinsAndPatchesApp/index.html"
+  },
+  {
+    "revision": "07be107db1cc97e8f922",
+    "url": "/pinsAndPatchesApp/static/css/main.5bed9a17.chunk.css"
+  },
+  {
+    "revision": "5d295ba12c55d854c05d",
+    "url": "/pinsAndPatchesApp/static/js/2.bc6f3137.chunk.js"
+  },
+  {
+    "revision": "7f84d11092c10d3a43893d78f68c23cb",
+    "url": "/pinsAndPatchesApp/static/js/2.bc6f3137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07be107db1cc97e8f922",
+    "url": "/pinsAndPatchesApp/static/js/main.7d89b641.chunk.js"
+  },
+  {
+    "revision": "34d4a67949f76d206774",
+    "url": "/pinsAndPatchesApp/static/js/runtime-main.e02ba560.js"
+  }
+]);
